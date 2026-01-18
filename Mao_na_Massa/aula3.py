d921ffd8ad44e35b06d3254b0b3c6badd12fa1d4
Leo33dvs <emidioleonardo33@gmail.com>
2026-01-17 20:54:10 -0500

    terminei aula 3

--- Mao_na_Massa/aula3.py
+++ Mao_na_Massa/aula3.py
@@ -5,9 +5,17 @@ Tipo de Tipagem = Dinâmica / forte
 str -> string -> texto
 Strings são textos que estão dentro de aspas
 """
-
+print(1234)
 
 # Aspas simples
-
+print('Leonardo de lima' , sep="*")#sep(separador)
 # Aspas duplas
 
+print("Leonardo de lima em busca de conhecimento")
+print(1,'Leonardo S"emidio"')
+
+# Escape
+print("Leonardo \"emidio")
+
+# r
+print(r"victor \"leonardo\"")
